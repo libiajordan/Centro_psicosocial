c8a7314ba1d0f991032f2ccf873576d1d00f809d
libiajordan <144961330+libiajordan@users.noreply.github.com>
2023-11-21 17:50:29 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Centro de Atención Psicosocial </title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  
+  <link href="assets/img/Logo.webp" rel="icon">
+  <link href="assets/img/uniclaretiana.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="stylesheet" href="/hs/hsstatic/ContentIcons/static-1.85/fontawesome/5.0.10/css/fontawesome-all.min.css">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <!-- Agrega las referencias a Bootstrap CSS y JS -->
+  <link rel="stylesheet" href="estilos.css"> <!-- Enlaza a tu archivo CSS personalizado -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+  
+
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/inicio.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top  header-transparent ">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h6><a href="index.html"> <img src="assets/img/logo-1.png" alt="" class="img-fluid"> CENTRO DE ATENCIÓN PSICOSOCIAL </a></h6>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Inicio</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Politicas</a></li>
+          <li><a class="nav-link scrollto" href="https://www.uniclaretiana.edu.co/">Uniclaretiana</a></li>
+          <li><a class="getstarted scrollto" href="ingreso/ingreso.php">Ingresar</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+ 
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1" data-aos="fade-up">
+          <div>
+            <h1>Centro de atención psicosocial - Uniclaretiana</h1>
+            <h2>El centro de atención psicosocial Uniclaretiana nace como una necesidad que responde al componente institucional, extensión que articula la academia y el sector externo, para contribuir a la solución de problemáticas sociales, económicas, culturales ambientales y políticas.</h2>
+            <a href="#main" class="boton"> Saber más </a>
+            <a href="#contact" class="boton">Contactanos </a>
+          </div>
+        </div>
+        <div class="col-lg-6 d-lg-flex flex-lg-column align-items-stretch order-1 order-lg-2 hero-img" data-aos="fade-up">
+          <img src="assets/img/img-1.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= App Features Section ======= -->
+    <section id="features" class="features">
+      <div class="container">
+
+        <div class="section-title">
+          <h2> Conoce más </h2>
+          <p>Ciñéndose a la política institucional de inclusión, humanización pretende brindar servicios psicosociales que den la posibilidad de atender a las necesidades de nuestra comunidad circundante, en este sentido los servicios que ofrecemos a través de nuestros estudiantes practicantes bajo la supervisión de docentes. Contamos con: 
+            
+
+          <div class="row no-gutters">
+          <div class="col-xl-7 d-flex align-items-stretch order-2 order-lg-1">
+            <div class="content d-flex flex-column justify-content-center">
+              <div class="container">  
+                <div class="row">
+                  <div class="col-md-6">   
+                    <div id="serviceCarousel" class="carousel slide" data-ride="carousel">
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <ul class="list-group">
+                            <li class="list-group-item">Procesos psicoterapéuticos en psicología individual y/o familiar</li>
+                          </ul>
+                          <ul class="list-group">
+                            <li class="list-group-item">Entrenamiento en habilidades sociales e inteligencia emocional</li>
+                          </ul>
+                        </div>
+                        <div class="carousel-item">
+                          <ul class="list-group">
+                            <li class="list-group-item">Evaluación neuropsicológica</li>
+                          </ul>
+                          <ul class="list-group">
+                            <li class="list-group-item">Elaboración de duelo ante cualquier perdida</li>
+                          </ul>
+                          <ul class="list-group">
+                            <li class="list-group-item">talleres de fortalecimiento personal y motivacional </li>
+                          </ul>  
+                          
+                        </div>
+                        <div class="carousel-item">
+                          <ul class="list-group">
+                            <li class="list-group-item">Acompañamiento por el area de trabajo social </li>
+                          </ul>
+                          <ul class="list-group">
+                            <li class="list-group-item">Procesos de selección de personal </li>
+                          </ul>
+                          <ul class="list-group">
+                            <li class="list-group-item">Procesos de formación y capacitación para empresas, instituciones publicas y privadas </li>
+                          </ul>  
+                          
+                        </div>
+                        <div class="carousel-item">
+                          <ul class="list-group">
+                            <li class="list-group-item">Acompañamiento integral a la familia</li>
+                          </ul>
+                          <ul class="list-group">
+                            <li class="list-group-item">Orientación vocacional </li>
+                          </ul>  
+                          <ul class="list-group">
+                            <li class="list-group-item">Acompañamiento psicologico individual y grupal </li>
+                          </ul>
+                          
+                        </div>
+
+                        <div class="carousel-item">
+                          <ul class="list-group">
+                            <li class="list-group-item">Talleres y seminarios en diferentes temáticas</li>
+                          </ul>
+                          <ul class="list-group">
+                            <li class="list-group-item"> Visitas domiciliarias </li>
+                          </ul>  
+                          <ul class="list-group">
+                            <li class="list-group-item">Brigadas barriales y comunitarias </li>
+                          </ul>
+                          
+                        </div>
+                        
+                        
+
+
+                      </div>
+                      <a class="carousel-control-prev" href="#serviceCarousel" role="button" data-slide="prev"> 
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span> 
+                        <span class="sr-only">Anterior</span>
+                      </a>
+                      <a class="carousel-control-next" href="#serviceCarousel" role="button" data-slide="next">   
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span> 
+                        <span class="sr-only">Siguiente</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="col-xl-5 d-flex align-items-stretch order-1 order-lg-2">    
+            <div id="imageCarousel" class="carousel slide" data-ride="carousel">  
+              <div class="carousel-inner">    
+                <div class="carousel-item active">  
+                  <img src="assets/img/1.jpg" class="d-block w-100 carousel-image"  alt="Imagen 1">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/img/2.jpg"" class="d-block w-100 carousel-image"  alt="Imagen 2">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/img/3.jpg"" class="d-block w-100 carousel-image"  alt="Imagen 3">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/img/4.jpg"" class="d-block w-100 carousel-image"  alt="Imagen 4">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/img/5.jpg"" class="d-block w-100 carousel-image"  alt="Imagen 5">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/img/6.jpg"" class="d-block w-100 carousel-image"  alt="Imagen 6">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/img/7.jpg"" class="d-block w-100 carousel-image"  alt="Imagen 7">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/img/8.jpg"" class="d-block w-100 carousel-image"  alt="Imagen 8">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/img/9.jpg"" class="d-block w-100 carousel-image"  alt="Imagen 9">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/img/10.jpg"" class="d-block w-100 carousel-image"  alt="Imagen 10  ">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/img/11.jpg"" class="d-block w-100 carousel-image"  alt="Imagen 11  ">
+                </div>
+                
+
+              </div>
+
+              <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev"> 
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span> 
+                <span class="sr-only">Anterior</span>
+              </a>
+
+              <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next"> 
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Siguiente</span>
+              </a>
+            </div>
+          </div>
+          
+      
+        </div>
+
+        </div> 
+        
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+        
+
+
+
+      </div>
+    </div>
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Contactanos</h2>
+          </div>
+
+        <div class="row">
+
+          <div class="col-lg-6">
+            <div class="row">
+              <div class="col-lg-6 info">
+                <i class="bx bx-map"></i>
+                <h4>Dirección</h4>
+                <p>Cra 3ª Nº 29 - 68,<br> frente aguas del atrato- barrio Cristo Rey, <br> Quibdó, Chocó</p>
+              </div>
+              <div class="col-lg-6 info">
+                <i class="bx bx-phone"></i>
+                <h4> Llamanos </h4>
+                <p>+57 3127799868</p>
+              </div>
+              <div class="col-lg-6 info">
+                <i class="bx bx-envelope"></i>
+                <h4>Email</h4>
+                <p>caps@uniclaretiana.edu.co</p>
+              </div>
+              <div class="col-lg-6 info">
+                <h4>Coordinadora</h4>
+                <p>Lelia Martinez Cuesta, <br> Coordinadora Centro de Atención Psicosocial </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form" data-aos="fade-up">
+              <div class="form-group">
+                <input placeholder="Tu nombre" type="text" name="name" class="form-control" id="name" required>
+              </div>
+              <div class="form-group mt-3">
+                <input placeholder="Tu email" type="email" class="form-control" name="email" id="email" required>
+              </div>
+              <div class="form-group mt-3">
+                <input placeholder="rol" type="text" class="form-control" name="subject" id="subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea placeholder="Mensaje" class="form-control" name="message" rows="5" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Cargando</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Tu mensaje ha sido recibido</div>
+              </div>
+              <div class="text-center"><button type="submit">Enviar mensaje</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </div>
+  
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <img src="assets/img/fucla.png" class="img-fluid" height="2px"> 
+            
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <br> <br> <br> <h4>Centro de Atención Psicosocial</h4>
+          </div>
+
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <div class="social-links mt-3">
+              <br> <br> <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-4">
+      
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/ -->
+        
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
